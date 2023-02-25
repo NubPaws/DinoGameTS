b78995e554df98699101786a97f8a9ffb61da014
NubPaws <DXroomram@gmail.com>
2023-02-26 01:57:29 +0200

    Created the Graphics wrapper class

---
+++ public/DinoGame.js
@@ -0,0 +1,4 @@
+import { Color } from "./graphics/Color.js";
+import { Graphics } from "./graphics/Graphics.js";
+let gfx = new Graphics("gameCanvas");
+gfx.fillBackground(Color.Cyan);
---
+++ src/DinoGame.ts
@@ -0,0 +1,6 @@
+import { Color } from "./graphics/Color.js";
+import { Graphics } from "./graphics/Graphics.js";
+
+let gfx = new Graphics("gameCanvas");
+
+gfx.fillBackground(Color.Cyan);
