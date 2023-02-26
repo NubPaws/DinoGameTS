53f9a8a7d971adb7328b6d09dd72552dbfaefb97
NubPaws <DXroomram@gmail.com>
2023-02-26 13:55:13 +0200

    Created the gameloop

--- src/DinoGame.ts
+++ src/DinoGame.ts
@@ -1,6 +1,23 @@
+import { GameLoop } from "./engine/GameLoop.js";
 import { Color } from "./graphics/Color.js";
 import { Graphics } from "./graphics/Graphics.js";
 
 let gfx = new Graphics("gameCanvas");
 
 gfx.fillBackground(Color.Cyan);
+
+let i = 0;
+const colors = [Color.Black, Color.Brown];
+
+function update() {
+	console.log("Something")
+}
+
+function render() {
+	gfx.fillBackground(colors[i++ % 2]);
+}
+
+let looper = GameLoop.instance;
+looper.updateFunction = update;
+looper.renderFunction = render;
+looper.start();
